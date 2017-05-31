cec90367dbfbf878bda6d7cac5ad5c5adf645a20
jiesenboor <jiesenboor@gmail.com>
2017-05-31 17:18:12 +0800

    修改配置

--- simple-config/simple-config-client/src/main/java/com/sen/SimpleConfigClientApplication.java
+++ simple-config/simple-config-client/src/main/java/com/sen/SimpleConfigClientApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimpleConfigClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleConfigClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleConfigClientApplication.class, args);
+    }
 }
